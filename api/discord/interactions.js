3801b280094239f7011347e896220b984bb762c2
Raphael <raphael@alsacreations.fr>
2026-04-14 20:06:38 +0200

    style(interactions): remplacer l'icône du titre des meilleurs joueurs de la famille

--- api/discord/interactions.js
+++ api/discord/interactions.js
@@ -1079,7 +1079,7 @@ export default async function handler(req, res) {
             throw new Error("Impossible de déterminer la saison cible.");
           }
 
-          title = `<:trophy:1493670704644751390>Meilleurs joueurs de la famille - saison précédente`;
+          title = `<:trophybig:1493673701554651277> Meilleurs joueurs de la famille - saison précédente`;
           footer = `Famille Resistance · Saison : S${selectedSeason}`;
           if (currentSeason != null && currentSeason !== selectedSeason) {
             footer += ` (la S${currentSeason} n'est pas terminée)`;
