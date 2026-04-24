0e7bf41f972747c9cf9a49de9350f3aa627559a9
Raphael <raphael@alsacreations.fr>
2026-04-24 15:36:17 +0200

    style(compare_snapshot_cache): améliore la lisibilité du code en réorganisant les imports et les logs

--- temp/compare_snapshot_cache.js
+++ temp/compare_snapshot_cache.js
@@ -1,7 +1,20 @@
 // Script pour comparer les tags du snapshot jeudi et du cache actuel
 // Usage : node temp/compare_snapshot_cache.js
-import fs from 'fs';
-const snap = Object.keys(JSON.parse(fs.readFileSync('./data/snapshots/LRQP20V9.json'))[0].days.find(d => d.warDay === 'thursday')._cumulFame);
-const cache = Object.keys(JSON.parse(fs.readFileSync('./frontend/dist/clan-cache/LRQP20V9.json')).membersRaw);
-console.log('Dans snapshot mais pas dans cache:', snap.filter(t => !cache.includes(t)));
-console.log('Dans cache mais pas dans snapshot:', cache.filter(t => !snap.includes(t)));
+import fs from "fs";
+const snap = Object.keys(
+  JSON.parse(fs.readFileSync("./data/snapshots/LRQP20V9.json"))[0].days.find(
+    (d) => d.warDay === "thursday",
+  )._cumulFame,
+);
+const cache = Object.keys(
+  JSON.parse(fs.readFileSync("./frontend/dist/clan-cache/LRQP20V9.json"))
+    .membersRaw,
+);
+console.log(
+  "Dans snapshot mais pas dans cache:",
+  snap.filter((t) => !cache.includes(t)),
+);
+console.log(
+  "Dans cache mais pas dans snapshot:",
+  cache.filter((t) => !snap.includes(t)),
+);
